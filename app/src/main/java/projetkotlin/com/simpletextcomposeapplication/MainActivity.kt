7eff4c6534e4cb25dd1e065128e857dfd2b5d8ec
unidesk <vinicius.nogueira@unidesk.com.br>
2022-11-17 17:02:31 -0300

    Seção 2 aula 17 - botões

--- app/src/main/java/projetkotlin/com/simpletextcomposeapplication/MainActivity.kt
+++ app/src/main/java/projetkotlin/com/simpletextcomposeapplication/MainActivity.kt
@@ -1,41 +1,39 @@
 package projetkotlin.com.simpletextcomposeapplication
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import projetkotlin.com.simpletextcomposeapplication.ui.theme.SimpleTextComposeApplicationTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            SimpleTextComposeApplicationTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    Greeting("Android")
-                }
-            }
+            GreetingButton()
         }
     }
 }
 
 @Composable
-fun Greeting(name: String) {
+fun GreetingText(name: String) {
     Text(text = "Hello $name!")
+}
+
+@Composable
+fun GreetingButton(){
+    Button(onClick = { }) {
+        GreetingText(name = "Button!!")
+        GreetingText(name = "Inovando Button!!")
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun DefaultPreview() {
-    SimpleTextComposeApplicationTheme {
-        Greeting("Android")
-    }
+fun DefaultPreviewMainActivity() {
+    GreetingButton()
 }
