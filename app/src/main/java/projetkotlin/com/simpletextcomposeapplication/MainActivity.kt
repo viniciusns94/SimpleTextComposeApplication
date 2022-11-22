311e2bf42f3f41ff3abfb083684834b07e053b59
unidesk <vinicius.nogueira@unidesk.com.br>
2022-11-22 16:35:04 -0300

    Seção 2 aula 22 - estilos

--- app/src/main/java/projetkotlin/com/simpletextcomposeapplication/MainActivity.kt
+++ app/src/main/java/projetkotlin/com/simpletextcomposeapplication/MainActivity.kt
@@ -5,13 +5,19 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.TextUnit
 
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -24,12 +30,15 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun GreetingText(name: String) {
-    Text(text = "Hello $name!",
+    Text(
+        text = "Hello $name!",
         modifier = Modifier
             .clickable(onClick = { })
             .width(200.dp)
             .height(240.dp)
-            .padding(all = 24.dp)
+            .padding(all = 24.dp),
+        style = MaterialTheme.typography.headlineSmall,
+        fontWeight = FontWeight.SemiBold
     )
 }
 
